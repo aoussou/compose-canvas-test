f3f24a939958e0f913dd7f36682bb809a0f8815d
aoussou <26421195+aoussou@users.noreply.github.com>
2022-08-19 19:34:19 +0900

    Add files via upload

---
+++ generated/source/buildConfig/debug/org/jetbrains/chat/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.jetbrains.chat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.jetbrains.chat";
+  public static final String BUILD_TYPE = "debug";
+}
